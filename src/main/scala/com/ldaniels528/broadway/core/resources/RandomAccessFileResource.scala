113055e1e3bef28685874e211c72229bbbd4284c
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-01-05 11:42:36 -0800

    added a random access file resource

---
+++ src/main/scala/com/ldaniels528/broadway/core/resources/RandomAccessFileResource.scala
@@ -0,0 +1,47 @@
+package com.ldaniels528.broadway.core.resources
+
+import java.io.{File, RandomAccessFile}
+
+import scala.collection.concurrent.TrieMap
+
+/**
+ * Random Access File Resource
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+class RandomAccessFileResource(val path: String) {
+  private val file = new File(path).getCanonicalFile
+  private val raf = new RandomAccessFile(file, "rw")
+
+  /**
+   * Writes the given bytes to the underlying file at the given offset
+   * @param offset the given offset
+   * @param bytes the given array of bytes
+   */
+  def write(offset: Long, bytes: Array[Byte]) {
+    raf.seek(offset)
+    raf.write(bytes)
+  }
+
+  /**
+   * Writes a line of text to the underlying file
+   * @param line the given line of text
+   */
+  def write(line: String) = raf.writeChars(line)
+
+}
+
+/**
+ * Random Access File Resource Singleton
+ * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+ */
+object RandomAccessFileResource {
+  private val resources = TrieMap[File, RandomAccessFileResource]()
+
+  def apply(path: String): RandomAccessFileResource = {
+    val file = new File(path).getCanonicalFile
+    resources.getOrElseUpdate(file, new RandomAccessFileResource(path))
+  }
+
+  def unapply(resource: RandomAccessFileResource) = Option(resource.path)
+
+}
