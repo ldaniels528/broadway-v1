93cd73b7d17484990843a679fc2fb52421369e1b
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-01-04 20:59:49 -0800

    cleanup and documentation updates

--- src/main/scala/com/ldaniels528/broadway/BroadwayNarrative.scala
+++ src/main/scala/com/ldaniels528/broadway/BroadwayNarrative.scala
@@ -2,7 +2,6 @@ package com.ldaniels528.broadway
 
 import com.ldaniels528.broadway.core.resources._
 import com.ldaniels528.broadway.server.ServerConfig
-import org.slf4j.LoggerFactory
 
 import scala.language.implicitConversions
 
@@ -11,7 +10,6 @@ import scala.language.implicitConversions
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 class BroadwayNarrative(val config: ServerConfig, val name: String) {
-  private lazy val logger = LoggerFactory.getLogger(getClass)
   private implicit val executionContext = config.system.dispatcher
   private var executable: Option[ReadableResource => Unit] = None
 
@@ -19,23 +17,11 @@ class BroadwayNarrative(val config: ServerConfig, val name: String) {
    * Setups the actions that will occur upon start of the topology
    * @param block the executable block
    */
-  def onStart(block: ReadableResource => Unit) {
-    this.executable = Option(block)
-  }
+  def onStart(block: ReadableResource => Unit) = executable = Option(block)
 
   /**
    * Starts executing the topology
    */
-  def start(resource: ReadableResource) {
-    // capture the start time
-    val startTime = System.currentTimeMillis()
-
-    // execute the topology
-    executable.foreach(_(resource))
-
-    // report the total runtime in milliseconds to the operator
-    val elapsedTime = System.currentTimeMillis() - startTime
-    logger.info(s"Processed '$name' in $elapsedTime msec")
-  }
+  def start(resource: ReadableResource) = executable.foreach(_(resource))
 
 }
--- src/main/scala/com/ldaniels528/broadway/core/narrative/NarrativeRuntime.scala
+++ src/main/scala/com/ldaniels528/broadway/core/narrative/NarrativeRuntime.scala
@@ -18,20 +18,47 @@ class NarrativeRuntime() {
   private val narratives = TrieMap[String, BroadwayNarrative]()
   private val properties = TrieMap[String, Properties]()
 
+  /**
+   * Creates or retrieves a feed based on the given descriptor
+   * @param fd the given [[FeedDescriptor]]
+   * @return the [[Feed]]
+   */
   def getFeed(fd: FeedDescriptor): Feed = {
     feeds.getOrElseUpdate(fd.uuid, Feed(fd.uuid, fd.name, fd.dependencies map (_.toFeed(this)), fd.narrative))
   }
 
+  /**
+   * Retrieves a set of properties by ID
+   * @param id the given property set ID
+   * @return an option of a set of [[Properties]]
+   */
   def getPropertiesByID(id: String): Option[Properties] = properties.get(id)
 
+  /**
+   * Retrieves or creates a Broadway narrative instance
+   * @param config the given [[ServerConfig]]
+   * @param td the given [[NarrativeDescriptor]]
+   * @return an outcome of a [[BroadwayNarrative]]
+   */
   def getNarrative(config: ServerConfig, td: NarrativeDescriptor): Try[BroadwayNarrative] = Try {
     narratives.getOrElseUpdate(td.id, instantiateTopology(config, td.className))
   }
 
+  /**
+   * Retrieves a narrative by its unique identifier
+   * @param id the given unique identifier
+   * @return the [[BroadwayNarrative]]
+   */
   def getNarrativeByID(id: String): BroadwayNarrative = {
-    narratives.get(id) orDie s"Topology ID '$id' not found"
+    narratives.get(id) orDie s"Narrative ID '$id' not found"
   }
 
+  /**
+   * Creates an instance of a Broadway Narrative
+   * @param config the given [[ServerConfig]]
+   * @param className the given class name
+   * @return the [[BroadwayNarrative]]
+   */
   private def instantiateTopology(config: ServerConfig, className: String) = {
     val topologyClass = Class.forName(className)
     val cons = topologyClass.getConstructors()(0)
