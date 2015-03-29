b8303b496749574cc9a0b4a12ebd27fd345af683
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-03-28 19:41:06 -0700

    added MongoDB source

--- src/main/scala/com/ldaniels528/broadway/datasources/mongodb/MongoDBSource.scala
+++ src/main/scala/com/ldaniels528/broadway/datasources/mongodb/MongoDBSource.scala
@@ -1,9 +1,41 @@
 package com.ldaniels528.broadway.datasources.mongodb
 
+import com.mongodb.MongoClientOptions
+import com.mongodb.casbah.Imports._
+import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
+
 /**
  * Represents a non-blocking MongoDB data source
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
-class MongoDBSource {
+class MongoDBSource(database: String,
+                    replicas: List[ServerAddress],
+                    defaultConcern: WriteConcern = WriteConcern.Safe) {
+
+  // register the time/date helpers
+  RegisterJodaTimeConversionHelpers()
+
+  /**
+   * Returns a new reference to the specified collection
+   */
+  def getCollection(name: String)(implicit conn: MongoConnection) = conn(database)(name)
+
+  /**
+   * Creates a new database connection
+   */
+  def getConnection = {
+    // create the options
+    val options = MongoClientOptions
+      .builder()
+      .connectionsPerHost(200)
+      .maxWaitTime(100)
+      .socketKeepAlive(true)
+      .threadsAllowedToBlockForConnectionMultiplier(50)
+      .build()
+
+    // create the connection
+    MongoConnection(replicas, new MongoOptions(options))
+  }
+
 
 }
